06982e6504b3c41b035cce14b3d9c4773f5c6bf4
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-10-08 12:20:35 +0200

    add new project

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -3,6 +3,7 @@ import { Project } from '@/data/types'
 import tutorial1 from '../../content/tutorial1.json'
 import figuren from '../../content/figuren.json'
 import dungeon from '../../content/dungeon.json'
+import brickbreaker from '../../content/brickbreaker.json'
 import gng from '../../content/gng.json'
 import empty from '../../content/leer.json'
 
@@ -10,6 +11,7 @@ export const projects: Project[] = [
   tutorial1,
   figuren,
   dungeon,
+  brickbreaker,
   gng,
   empty,
 ] as Project[]
